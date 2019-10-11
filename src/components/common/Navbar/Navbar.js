03fc68a0eb26cf5407bfdc0d4b45c3751d8b28b1
Shishak <semenshishak@gmail.com>
2019-10-11 14:51:42 +0300

    Update Navbar.js

--- src/components/common/Navbar/Navbar.js
+++ src/components/common/Navbar/Navbar.js
@@ -15,7 +15,7 @@ import {
 
 import { ReactComponent as MenuIcon } from '@static/icons/menu.svg';
 
-const NAV_ITEMS = ['About', 'Brands', 'Team', 'FAQ'];
+const NAV_ITEMS = ['Межбанк', 'Помощь по подписке'];
 
 class Navbar extends Component {
   state = {
